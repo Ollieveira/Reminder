3f247aeb00689cab697c2aed8a38309adc81e287
Ollieveira1 <willysaaoliveira@gmail.com>
2025-06-24 10:59:33 -0300

    - Alteracao do asset da logo - Implementado tres contants para padronizacao de código para textos, cores e espacamentos - Ajustes na SplashView devido a mudanca do asset da Logo

---
+++ Reminder/Sources/Features/Constants/Color-Constants/Colors-Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Colors-Constants.swift
+//  Reminder
+//
+//  Created by Willys Oliveira on 24/06/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    
+    static let primaryRedBase = UIColor(cgColor: CGColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1))
+    static let secondaryRedLight = UIColor(cgColor: CGColor(red: 235/255, green: 60/255, blue: 77/255, alpha: 1))
+    static let primaryBlueBase = UIColor(cgColor: CGColor(red: 51/255, green: 79/255, blue: 220/255, alpha: 1))
+    static let secondaryBlueLight = UIColor(cgColor: CGColor(red: 110/255, green: 131/255, blue: 236/255, alpha: 1))
+}
---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by Willys Oliveira on 24/06/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    static let huge: CGFloat = 68
+    static let medium: CGFloat = 32
+    static let small: CGFloat = 16
+    static let tiny: CGFloat = 8
+    static let little: CGFloat = 4
+}
